640d560716328197cfeb26c2349d261eb8f70661
Dylan Jeffries <dylanj@me.com>
2024-06-26 16:17:46 -0700

    Functions for exploratory analysis

--- create_table.R
+++ create_table.R
@@ -2,6 +2,10 @@ create_table <- function(data, var1, var2) {
   table_result <- table(data[[var1]], data[[var2]])
   df_table <- as.data.frame.matrix(table_result)
   df_table["Total", ] <- colSums(df_table)
+  df_table[,1] <- as.integer(round(df_table[,1], 0))
+  df_table[,2] <- as.integer(round(df_table[,2], 0))
+  df_table$`Mortality Rate` <- (df_table[,1] / (df_table[,1] + df_table[,2])) * 100
+  df_table$`Mortality Rate`[is.nan(df_table$`Mortality Rate`)] <- 0
   table_with_total <- as.table(as.matrix(df_table))
-  return(table_with_total)
+  return(df_table)
 }
